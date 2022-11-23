3cbdee1b598c500c0f7eff5c9569de4951e9c63b
timur <timurka.zavyalov@mail.ru>
2022-11-23 18:55:24 +0500

    add: added logger in init

--- scripts_for_bash_with_inheritance/comparison_by_sha.py
+++ scripts_for_bash_with_inheritance/comparison_by_sha.py
@@ -22,27 +22,35 @@ def change_types_columns_and_replace_quot_marks(parsed_data: List[dict]) -> None
                     dict_data[key] = str(float(value))
 
 
-def read_csv_pandas(csv_file: str, is_download: bool = False) -> Tuple[DataFrame, str]:
+def read_csv_pandas(csv_file: str, base_name_csv_file: str, is_download: bool = False) -> Tuple[DataFrame, str]:
     df = pd.read_csv(csv_file, low_memory=False, dtype=str)
+    logger.info(f"{base_name_csv_file}: File has been read")
     if is_download:
         df = df.loc[:, ~df.columns.isin(["id", "original_file_name", "original_file_parsed_on"])]
     df = df.rename(columns=headers_eng)
     df.replace({np.NAN: None}, inplace=True)
+    logger.info(f"{base_name_csv_file}: Column names have been replaced")
     parsed_data = df.to_dict('records')
+    logger.info(f"{base_name_csv_file}: Dataframe have been converted to a list with dictionary")
     change_types_columns_and_replace_quot_marks(parsed_data)
+    logger.info(f"{base_name_csv_file}: Changed column types")
     df = pd.DataFrame(parsed_data)
+    logger.info(f"{base_name_csv_file}: List with dictionary converted to a dataframe")
     return df, hashlib.sha256(df.to_string().encode('utf-8')).hexdigest()
 
 
 def unzip_file(zip_file):
     with zipfile.ZipFile(zip_file, "r") as zf:
-        return [zf.extract(name, f"{os.path.dirname(sys.argv[1])}/csv") for name in zf.namelist()]
+        return [zf.extract(name, f"{os.path.dirname(sys.argv[1])}/csv") for name in sorted(zf.namelist(), reverse=True)]
 
 
 if __name__ == "__main__":
     upload_file, download_file = unzip_file(sys.argv[1])
-    df_upload, hashlib_upload = read_csv_pandas(upload_file)
-    df_download, hashlib_download = read_csv_pandas(download_file, True)
+    base_name_upload_file = os.path.basename(upload_file)
+    base_name_download_file = os.path.basename(download_file)
+    logger.info(f"Upload file: {base_name_upload_file}, Download file: {base_name_download_file}")
+    df_upload, hashlib_upload = read_csv_pandas(upload_file, base_name_upload_file)
+    df_download, hashlib_download = read_csv_pandas(download_file, base_name_download_file, True)
     if hashlib_upload == hashlib_download:
         logger.info("Хэш файлов одинаковый")
     else:
